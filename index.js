290e86246a8ffc69dd4025ff6514008ef8db5875
Bruno Borges <bruno.borges@microsoft.com>
2019-12-03 02:52:52 +0100

    fix console log

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const tc = require('@actions/tool-cache');
 
+/*
 function getFileEnding(file) {
     let fileEnding = '';
 
@@ -13,20 +14,23 @@ function getFileEnding(file) {
     } else if (file.endsWith('.7z')) {
         fileEnding = '.7z';
     } else {
-        throw new Error(`${file} has an unsupported file extension`);
+        throw new Error("${file} has an unsupported file extension");
     }
 
     return fileEnding;
 }
+*/
 
 try {
     const file = core.getInput('file');
-    console.log('File to extract: ${file}.');
+    console.log("File to extract: ${file}.");
 
     const dest = core.getInput('dest');
-    console.log('Destination: ${dest}.');
+    console.log("Destination: ${dest}.");
 
     const fileEnding = file.slice((file.lastIndexOf('.') - 1 >>> 0) + 2);
+    console.log("File Extension: ${fileEnding}.");
+
     if ('.tar' === fileEnding || '.tar.gz' === fileEnding) {
         tc.extractTar(file, dest);
     } else if ('.zip' === fileEnding) {
@@ -34,7 +38,7 @@ try {
     } else if ('.7z' === fileEnding) {
         tc.extract7z(file, dest);
     } else {
-        throw new Error('${file} has an unsupported file extension.');
+        throw new Error("${file} has an unsupported file extension.");
     }
 } catch (error) {
     core.setFailed(error.message);
